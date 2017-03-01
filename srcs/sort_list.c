8c62bbf951e603ece99a770713c14a02c2256cd0
jdereynal <jacques.de-reynal@epitech.eu>
2017-03-01 17:25:51 +0100

    sort start

--- srcs/sort_list.c
+++ srcs/sort_list.c
@@ -5,9 +5,30 @@
 ** Login   <jack@epitech.net>
 **
 ** Started on  Wed Mar  1 16:52:50 2017 jack
-** Last update Wed Mar  1 16:55:42 2017 jack
+** Last update Wed Mar  1 17:25:36 2017 jack
 */
 
 #include "tetris.h"
 
-void	sort_list()
+void		swap(t_list *my_list, t_list *tmp, t_list *tmp2)
+{
+}
+
+void		sort_list(t_list *my_list)
+{
+  t_list	*tmp;
+  t_list	*tmp2;
+
+  tmp = my_list->next;
+  while (tmp)
+    {
+      tmp2 = my_list->next;
+      while (tmp2)
+	{
+	  if (my_strcmp(tmp2->obj.name, tmp->obj.name) < 0)
+	    swap(my_list, tmp, tmp2);
+	}
+      tmp2 = tmp2->next;
+    }
+  tmp = tmp->next;
+}
