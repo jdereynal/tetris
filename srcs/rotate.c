2cabcdb4bb7e4a311af9ebfcfac18777941d8833
jdereynal <jacques.de-reynal@epitech.eu>
2017-03-14 10:14:12 +0100

    rotate

--- srcs/rotate.c
+++ srcs/rotate.c
@@ -5,36 +5,9 @@
 ** Login   <jack@epitech.net>
 **
 ** Started on  Mon Feb 27 19:16:50 2017 jack
-** Last update Tue Feb 28 16:43:03 2017 jack
+** Last update Tue Mar 14 10:13:57 2017 jack
 */
 
 #include <stdlib.h>
 #include "tetris.h"
 
-char	**rotate(char **shape)
-{
-  int	i;
-  int	j;
-  char	**result;
-  char	*tmp;
-
-  if ((result = malloc(sizeof(char *) * (biggest_line(shape) + 1))) == NULL)
-    return (NULL);
-  if ((tmp = malloc(sizeof(char) * (my_strlen(shape[0] + 1)))) == NULL)
-    return (NULL);
-  i = 0;
-  while (i < my_strlen(shape[0]))
-    {
-      j = 0;
-      while (j < my_tab_length(shape))
-	{
-	  tmp[j] = result[j][i];
-	  j++;
-	}
-      tmp[j] = '\0';
-      result[i] = reverse(tmp);
-      i++;
-    }
-  result[i] = 0;
-  return (result);
-}
